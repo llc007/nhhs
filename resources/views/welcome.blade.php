4b70d0dc47cde62acee746e12c049ae7a751d3f6
llc007 <llc007.1@gmail.com>
2019-02-26 17:37:01 -0300

    Titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema cursos
                 </div>
 
                 <div class="links">
